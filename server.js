0d0649d45b10806a36b985272c028ad223dd2f7a
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-03-27 12:46:57 +0530

    feat: add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import cors from "cors";
 const { json, urlencoded } = bodyParser;
 import detect_POST from "./routes/detect_POST.js";
 
 const app = express();
 
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("tiny"));
