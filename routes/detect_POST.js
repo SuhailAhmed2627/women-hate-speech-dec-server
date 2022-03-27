ae09303ef85e9941e09e7b9d3062f0c7f5652db7
suhailahmed <suhailahmed2001sam@gmail.com>
2022-03-27 18:18:18 +0530

    ujiubujy

--- routes/detect_POST.js
+++ routes/detect_POST.js
@@ -9,15 +9,29 @@ const options = {
 };
 
 const detect_POST = async (req, res) => {
+	if (!req.body || !req.body.username || req.body.username === "") {
+		return res.status(400).json({ message: "Invalid Request" });
+	}
+
+	let userData, tweetsData;
+
 	const getUserIDEndpoint = `https://api.twitter.com/2/users/by?usernames=${req.body.username}`;
-	const user = await fetch(getUserIDEndpoint, options);
-	const userData = await user.json();
+	try {
+		const user = await fetch(getUserIDEndpoint, options);
+		userData = await user.json();
+	} catch {
+		return res.status(400).json({ message: "Error Fetching User Details" });
+	}
 
 	const getMentionsEndpoint = `https://api.twitter.com/2/users/${userData.data[0].id}/mentions`;
-	const tweets = await fetch(getMentionsEndpoint, options);
-	const tweetsData = await tweets.json();
+	try {
+		const tweets = await fetch(getMentionsEndpoint, options);
+		tweetsData = await tweets.json();
+	} catch {
+		return res.status(400).json({ message: "Error Fetching Tweet Details" });
+	}
 
-	res.status(200).json(tweetsData);
+	return res.status(200).json(tweetsData);
 };
 
 export default detect_POST;
